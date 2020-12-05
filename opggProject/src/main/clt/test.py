c42b1663902af5833371224e434ab9a88829b85f
메돌이 <68496830+rrilla@users.noreply.github.com>
2020-12-05 19:17:04 +0900

    multiSearch80% (#8)
    Co-authored-by: jaehak9078 <jaehak9078@gmail.com>

---
+++ opggProject/src/main/clt/test.py
@@ -0,0 +1,8 @@
+import java.util.ArrayList as ArrayList
+
+def testFunc():
+	arr = ArrayList()
+	arr.add(1)
+	arr.add(2)
+	arr.add(3)
+	return arr
--- opggProject/src/main/java/com/example/opggProject/controller/TestController.java
+++ opggProject/src/main/java/com/example/opggProject/controller/TestController.java
@@ -1,16 +1,60 @@
 package com.example.opggProject.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.python.core.PyArray;
+import org.python.core.PyFunction;
+import org.python.core.PyInteger;
+import org.python.core.PyList;
+import org.python.core.PyObject;
+import org.python.core.PyString;
+import org.python.util.PythonInterpreter;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import lombok.extern.java.Log;
+
+
+@Log
 @Controller
 public class TestController {
+	private PythonInterpreter intPre;
 	
+
+	@GetMapping("/test")
+	public String getTest(Model model) {
+		intPre = new PythonInterpreter();
+		intPre.execfile("src/main/clt/test.py");
+		PyFunction pyFunction = (PyFunction) intPre.get("testFunc",PyFunction.class);
+		
+		
+		PyObject pyobj = pyFunction.__call__();
+		
+		model.addAttribute("pytestResult", pyobj.toString());
+		log.info(pyobj.toString());
+		return "test";
+	}
 	
+	@GetMapping("/test2")
+	public String getTest2(Model model) {
+		intPre = new PythonInterpreter();
+		intPre.execfile("src/main/clt/multiSearch.py");
+		PyFunction pyFunction = (PyFunction) intPre.get("multiSearch",PyFunction.class);
+		
+		String name = "ZED99";
+		PyObject pyobj = pyFunction.__call__(new PyString(name));
+//		ArrayList<String> test = pyobj;
+		model.addAttribute("pytestResult", pyobj.toString());
+		log.info(pyobj.toString());
+		return "test2";
+
 	@GetMapping("/main")
 	public String test() {
 		return "main";
+
 	}
 	
 	@GetMapping("/loginForm")
