cb89be7551609e2f72db0227ec7a9ae3182ec025
메돌이 <68496830+rrilla@users.noreply.github.com>
2020-12-05 19:11:28 +0900

    Revert "Sub/jaehak (#5)" (#10)
    This reverts commit da9797ec973deb9815aef3f6a27912792ff19752.

--- opggApp/app/src/main/java/com/project/opggapp/task/IP.java
+++
@@ -1,8 +0,0 @@
-package com.project.opggapp.task;
-
-public class IP {
-    //public final static String ip = "172.30.1.58"; // IP - 집
-    //public final static String ip = "10.100.102.15"; // IP - 학원
-    public final static String ip = "10.0.2.2";    //애뮬레이터에서 로컬서버주소
-    public final static String serverUrl = "http://" + ip + ":8000/";
-}
