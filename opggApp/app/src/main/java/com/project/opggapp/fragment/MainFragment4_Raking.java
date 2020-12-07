50da18e2157a98a2f12a004c9a7e5def35629b6b
한재현 <68496830+rrilla@users.noreply.github.com>
2020-12-07 17:59:21 +0900

    메인 디자인 끝, 랭킹중 (#12)

--- opggApp/app/src/main/java/com/project/opggapp/fragment/MainFragment4_Raking.java
+++ opggApp/app/src/main/java/com/project/opggapp/fragment/MainFragment4_Raking.java
@@ -1,21 +1,20 @@
 package com.project.opggapp.fragment;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.fragment.app.Fragment;
+
 import com.project.opggapp.R;
 
-public class NavFragment1 extends Fragment {
+public class MainFragment4_Raking extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nav1, container, false);
+        return inflater.inflate(R.layout.fragment_main4_raking, container, false);
     }
 }
